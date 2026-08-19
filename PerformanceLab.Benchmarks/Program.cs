024cb374cce284217b1f242df7651b09f6bfd65e
Leandro <leandro.mm17@gmail.com>
2026-08-19 11:29:07 -0300

    feature: cria classes em PerformanceLab.Benchmaks

--- PerformanceLab.Benchmarks/Program.cs
+++ PerformanceLab.Benchmarks/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using BenchmarkDotNet.Running;
+using PerformanceLab.Benchmark.Classes;
+
+Console.WriteLine("Hello, World!");
+
+var summaryStringProcessing = BenchmarkRunner.Run<StringProcessingBenchmark>();
+var summaryCalcProcessing = BenchmarkRunner.Run<CalcBenchmark>();
+
